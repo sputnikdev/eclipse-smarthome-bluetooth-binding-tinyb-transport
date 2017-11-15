9c81dc5d3d2a110eb723418b7694a745b514d865
Vlad Kolotov <vkolotov@gmail.com>
2017-11-15 16:03:49 +1300

    WIP: Migrating from the official ESH repository Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

---
+++ src/main/java/org/sputnikdev/bluetooth/manager/transport/tinyb/activator/TinyBActivator.java
@@ -0,0 +1,21 @@
+package org.sputnikdev.bluetooth.manager.transport.tinyb.activator;
+
+import org.osgi.framework.BundleContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.sputnikdev.bluetooth.manager.transport.BluetoothObjectFactory;
+import org.sputnikdev.bluetooth.manager.transport.tinyb.TinyBFactory;
+
+@Component(immediate = true, name = "binding.bluetooth.transport.tinyb.activator")
+public class TinyBActivator {
+
+    @Activate
+    public void activate(BundleContext bundleContext) {
+        if (!TinyBFactory.loadNativeLibraries()) {
+            throw new IllegalStateException("Could not load native libraries for TinyB");
+        }
+        bundleContext.registerService(BluetoothObjectFactory.class.getName(), new TinyBFactory(), null);
+    }
+
+}
